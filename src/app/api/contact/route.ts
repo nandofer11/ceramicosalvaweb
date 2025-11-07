5e26d2f67e5dafe8cf711899e902ce6096810f73
Orlando Fernández <nandofer.11@hotmail.com>
2025-11-06 22:37:29 -0500

    Actualiza la configuración de Nodemailer para usar el host y puerto específicos de Gmail.

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -3,7 +3,9 @@ import nodemailer from 'nodemailer';
 
 // Configuración de Nodemailer para VPS
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
